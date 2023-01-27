ef23a6835b814055711090f37da4e5ed7cf42985
Martin <rmsk2@gmx.de>
2023-01-27 21:17:41 +0100

    Implement BCD arithmetic on a logical level

--- cpu/instructions_arith.go
+++ cpu/instructions_arith.go
@@ -80,85 +80,77 @@ func (c *CPU6502) addBaseBin(val1, val2 uint8) uint8 {
 	return r
 }
 
-// See http://www.6502.org/tutorials/decimal_mode.html Appendix A
-func (cp *CPU6502) addBaseBcd6502(val1, val2 uint8) uint8 {
-	a := uint16(val1)
-	b := uint16(val2)
-	var c uint16 = 0
-
-	if (cp.Flags & Flag_C) != 0 {
-		c = 1
-	}
-
-	// 1a. AL = (A & $0F) + (B & $0F) + C
-	al := (a & 0x0F) + (b & 0x0F) + c
-	// 1b. If AL >= $0A, then AL = ((AL + $06) & $0F) + $10
-	if al >= 0x0A {
-		al = ((al + 0x06) & 0x0F) + 0x10
-	}
-	// 1c. A = (A & $F0) + (B & $F0) + AL
-	a = (a & 0xF0) + (b & 0xF0) + al
-	// 1d. Note that A can be >= $100 at this point
-	// 1e. If (A >= $A0), then A = A + $60
-	if a >= 0xA0 {
-		a = a + 0x60
-	}
-	// 1f. The accumulator result is the lower 8 bits of A
-	r := uint8(a & 0xFF)
-
-	if a >= 0x100 {
-		cp.Flags |= Flag_C
-	} else {
-		cp.Flags &= (^Flag_C)
-	}
-
-	// Calculate setting of N, V and Z flags, which are invalid but if
-	// we know how to do it in a precise manner why not implementing it
-	// correctly
-	cr := uint8(c)
-
-	//2a. AL = (A & $0F) + (B & $0F) + C
-	al2 := (val1 & 0x0F) + (val2 & 0x0F) + cr
-	//2b. If AL >= $0A, then AL = ((AL + $06) & $0F) + $10
-	if al2 >= 0x0A {
-		al2 = ((al2 + 0x06) & 0x0F) + 0x10
-	}
-	//2c. A = (A & $F0) + (B & $F0) + AL, using signed (twos complement) arithmetic
-	temp := (val1 & 0xF0) + (val2 & 0xF0) + al2
-
-	//2e. The N flag result is 1 if bit 7 of A is 1, and is 0 if bit 7 if A is 0
-	if (temp & 0x80) != 0 {
-		cp.Flags |= Flag_N
-	} else {
-		cp.Flags &= (^Flag_N)
-	}
-	//2f. The V flag result is 1 if A < -128 or A > 127, and is 0 if -128 <= A <= 127
-	v := int8(temp)
-	if (v < -128) || (v > 127) {
-		cp.Flags |= Flag_V
-	} else {
-		cp.Flags &= (^Flag_V)
-	}
-
-	// Zero flag is set as in binary mode
-	a = uint16(val1)
-	t2 := a + b + c
-	if uint8(t2&0xFF) == 0 {
-		cp.Flags |= Flag_Z
-	} else {
-		cp.Flags &= (^Flag_Z)
-	}
-
-	return r
-}
-
 func (c *CPU6502) subBaseBin(val1, val2 uint8) uint8 {
 	// ... SBC simply takes the ones complement of the second value and then performs an ADC ...
 	// Source http://www.righto.com/2012/12/the-6502-overflow-flag-explained.html
 	return c.addBaseBin(val1, val2^0xFF)
 }
 
-// See http://www.6502.org/tutorials/decimal_mode.html Appendix A
+func (c *CPU6502) fromBCD(val1, val2 uint8) (uint8, uint8, uint8, uint8, uint8) {
+	loNibble1 := uint8(val1 & 0x0F)
+	hiNibble1 := uint8((val1 & 0xF0) >> 4)
+
+	loNibble2 := uint8(val2 & 0x0F)
+	hiNibble2 := uint8((val2 & 0xF0) >> 4)
+
+	var carry uint8 = 0
+	if (c.Flags & Flag_C) != 0 {
+		carry = 1
+	}
+
+	if (loNibble1 > 9) || (hiNibble1 > 9) || (loNibble2 > 9) || (hiNibble2 > 9) {
+		panic("Invalid BCD data")
+	}
+
+	return loNibble1, hiNibble1, loNibble2, hiNibble2, carry
+}
+
+func (c *CPU6502) toBCD(res uint8) uint8 {
+	res = res % 100
+	loDigit := res % 10
+	hiDigit := res / 10
+
+	return (hiDigit >> 4) | loDigit
+}
+
+// See http://www.6502.org/tutorials/decimal_mode.html
+// We implement a logically correct version of BCD. This routine does not simulate undocumented
+// behaviour. We therefore panic if the number is not valid BCD and we do not change any
+// flags other than the carry flag. On a 6502 the only documented behaviour is for
+// setting the carry flag.
+func (c *CPU6502) addBaseBcd6502(val1, val2 uint8) uint8 {
+	loNibble1, hiNibble1, loNibble2, hiNibble2, carry := c.fromBCD(val1, val2)
+
+	cr := hiNibble1*10 + loNibble1 + hiNibble2*10 + loNibble2 + carry
+
+	if (cr / 100) > 0 {
+		c.Flags |= Flag_C
+	} else {
+		c.Flags &= (^Flag_C)
+	}
+
+	return c.toBCD(cr % 100)
+}
+
+// See http://www.6502.org/tutorials/decimal_mode.html
+// We implement a logically correct version of BCD. This routine does not simulate undocumented
+// behaviour. We therefore panic if the number is not valid BCD and we do not change any
+// flags other than the carry flag. On a 6502 the only documented behaviour is for
+// setting the carry flag.
 func (c *CPU6502) subBaseBcd6502(val1, val2 uint8) uint8 {
-	return 0
+	loNibble1, hiNibble1, loNibble2, hiNibble2, carry := c.fromBCD(val1, val2)
+
+	b1 := int16(hiNibble1*10 + loNibble1)
+	b2 := int16(hiNibble2*10 + loNibble2)
+	cr := int16(1 - carry)
+
+	temp := b1 - b2 - cr
+
+	if temp < 0 {
+		c.Flags &= (^Flag_C)
+		return c.toBCD(uint8(temp + 100))
+	} else {
+		c.Flags |= Flag_C
+		return c.toBCD(uint8(temp))
+	}
 }
