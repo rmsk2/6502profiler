5410d027bb0d01080c18d36bf889d81339bc4a28
Martin <rmsk2@gmx.de>
2023-02-10 21:39:31 +0100

    Implement get and set memory functions

--- testprg/tests/test1.lua
+++ testprg/tests/test1.lua
@@ -1,6 +1,8 @@
 function arrange()
+    setmemory("10203040", 2048+3)
 end
 
 function assert()
-    return true
+    d = getmemory(2048+7, 4)
+    return d == "10203040"
 end
--- verifier/test_case.go
+++ verifier/test_case.go
@@ -2,6 +2,7 @@ package verifier
 
 import (
 	"6502profiler/cpu"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -80,10 +81,57 @@ func callAssert(L *lua.LState) (bool, error) {
 	return testRes, nil
 }
 
+type LuaCtx struct {
+	cpu *cpu.CPU6502
+}
+
+func (c *LuaCtx) RegisterFunctions(L *lua.LState) error {
+	L.SetGlobal("getmemory", L.NewFunction(c.GetMemory))
+	L.SetGlobal("setmemory", L.NewFunction(c.SetMemory))
+
+	return nil
+}
+
+func (c *LuaCtx) GetMemory(L *lua.LState) int {
+	laddr := L.ToInt(1)
+	memLen := L.ToInt(2)
+
+	data, err := c.cpu.CopyFromMem(uint16(laddr), uint16(memLen))
+	if err != nil {
+		panic("Unable to read memory")
+	}
+
+	dataStr := hex.EncodeToString(data)
+	L.Push(lua.LString(dataStr))
+
+	return 1
+}
+
+func (c *LuaCtx) SetMemory(L *lua.LState) int {
+	dataStr := L.ToString(1)
+	addr := L.ToInt(2)
+
+	data, err := hex.DecodeString(dataStr)
+	if err != nil {
+		panic("Unable to write memory")
+	}
+
+	err = c.cpu.CopyToMem(data, uint16(addr))
+	if err != nil {
+		panic("Uuable to write memory")
+	}
+
+	return 0
+}
+
 func (t *TestCase) Execute(cpu *cpu.CPU6502, assembler cpu.Assembler, testDir string) error {
 	binaryToTest, err := assembler.Assemble(t.AssemblerSource)
 	if err != nil {
 		return fmt.Errorf("unable to execute test case '%s': %v", t.Name, err)
+	}
+
+	ctx := &LuaCtx{
+		cpu: cpu,
 	}
 
 	loadAdress, _, err := cpu.Load(binaryToTest)
@@ -99,6 +147,11 @@ func (t *TestCase) Execute(cpu *cpu.CPU6502, assembler cpu.Assembler, testDir st
 	err = L.DoFile(scriptPath)
 	if err != nil {
 		return fmt.Errorf("unable to load test script: %v", err)
+	}
+
+	err = ctx.RegisterFunctions(L)
+	if err != nil {
+		return fmt.Errorf("unable to register Lua functions: %v", err)
 	}
 
 	fmt.Printf("Executing test case '%s' ... ", t.Name)
