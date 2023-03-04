fba17a3690a41805e7e84b72a4c80bb9b06f3938
Martin Grap <rmsk2@gmx.de>
2023-03-04 16:12:40 +0100

    Add printing a memory dump to profile command

--- commands/profile_command.go
+++ commands/profile_command.go
@@ -9,6 +9,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 const strategyMedian = "median"
@@ -29,6 +31,56 @@ func determineCutOffCalc(strategy *string, p float64) profiler.CutOffCalc {
 	return ctOff
 }
 
+func parseDumpParams(param string) (uint16, uint16, error) {
+	if param == "" {
+		return 0, 0, nil
+	}
+
+	r := regexp.MustCompile(`^([0-9]+):([0-9]+)$`)
+	matches := r.FindStringSubmatch(param)
+	if matches == nil {
+		return 0, 0, fmt.Errorf("unable to parse dump parameters")
+	}
+
+	dumpAddress, err := strconv.ParseUint(matches[1], 10, 16)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	dumpLen, err := strconv.ParseUint(matches[2], 10, 16)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if dumpLen == 0 {
+		return 0, 0, fmt.Errorf("length has to be at least 1")
+	}
+
+	dumpAddress16 := uint16(dumpAddress)
+	dumpLen16 := uint16(dumpLen)
+
+	if (dumpAddress16 + dumpLen16 - 1) < dumpAddress16 {
+		return 0, 0, fmt.Errorf("dump parameters result in end address being lower than start address")
+	}
+
+	return dumpAddress16, dumpLen16, nil
+}
+
+func DumpMemory(param string, cpu *cpu.CPU6502) error {
+	if param == "" {
+		return nil
+	}
+
+	dumpAddress, dumpLen, err := parseDumpParams(param)
+	if err != nil {
+		return err
+	}
+
+	memory.Dump(cpu.Mem, dumpAddress, dumpAddress+dumpLen-1)
+
+	return nil
+}
+
 func ProfileCommand(arguments []string) error {
 	var p float64
 	var labels map[uint16][]string
@@ -43,6 +95,7 @@ func ProfileCommand(arguments []string) error {
 	percentageCutOff := profileFlags.Uint("prcnt", 10, "Percentage used to determine cut off value")
 	strategy := profileFlags.String("strategy", strategyMedian, "Strategy to determine cutoff value")
 	configName := profileFlags.String("c", "", "Config file name")
+	dumpFlag := profileFlags.String("dump", "", "Dump memory after program has stopped. Format 'startaddr:len'")
 
 	if err = profileFlags.Parse(arguments); err != nil {
 		os.Exit(util.ExitErrorSyntax)
@@ -67,6 +120,10 @@ func ProfileCommand(arguments []string) error {
 
 	if *binaryFileName == "" {
 		return fmt.Errorf("no program specified")
+	}
+
+	if _, _, err := parseDumpParams(*dumpFlag); err != nil {
+		return err
 	}
 
 	if statisticRequested {
@@ -99,6 +156,11 @@ func ProfileCommand(arguments []string) error {
 		if err = profiler.DumpStatistics(processor.Mem, *outputFileName, labels, loadAddress, (loadAddress + progLen - 1), ctOff); err != nil {
 			return fmt.Errorf("problem generating output file: %v", err)
 		}
+	}
+
+	err = DumpMemory(*dumpFlag, processor)
+	if err != nil {
+		return err
 	}
 
 	return nil
--- memory/memory.go
+++ memory/memory.go
@@ -1,8 +1,8 @@
 package memory
 
 import (
-	"encoding/hex"
 	"fmt"
+	"strconv"
 )
 
 type Memory interface {
@@ -16,13 +16,56 @@ type Memory interface {
 }
 
 func Dump(m Memory, start uint16, end uint16) {
-	temp := make([]byte, end-start+1)
-	var index uint16 = 0
+	byteCount := 0
+	crlfWritten := false
+	printableChars := ""
 
-	for count := start; count <= end; count++ {
-		temp[index] = m.Load(count)
-		index++
+	lineDone := func() {
+		crlfWritten = true
+		byteCount = 0
+		fmt.Println(printableChars + "|")
 	}
 
-	fmt.Print(hex.Dump(temp))
+	for i := start; i <= end; i++ {
+		crlfWritten = false
+
+		if byteCount == 0 {
+			fmt.Printf("$%04x  ", i)
+			printableChars = " |"
+		}
+
+		if byteCount == 8 {
+			fmt.Printf(" ")
+		}
+
+		data := m.Load(i)
+
+		if (data < 128) && (strconv.IsPrint(rune(data))) {
+			printableChars += string([]byte{data})
+		} else {
+			printableChars += "."
+		}
+
+		fmt.Printf("%02x ", data)
+
+		if byteCount == 15 {
+			lineDone()
+		} else {
+			byteCount++
+		}
+	}
+
+	if !crlfWritten {
+		maxSpaces := "                                              "
+
+		plusOneSpace := 0
+		if byteCount <= 8 {
+			plusOneSpace++
+		}
+
+		fmt.Print(maxSpaces[:(16-byteCount)*3+plusOneSpace])
+		lineDone()
+	}
+
+	fmt.Printf("$%04x\n", end)
 }
