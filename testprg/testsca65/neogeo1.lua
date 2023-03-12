d8e0ae4c8c8f0b5e5e5313970637390fa72a62c1
Martin Grap <rmsk2@gmx.de>
2023-03-12 16:44:00 +0100

    make sure 65C02 instructions work with ca65

--- testprg/testsca65/neogeo1.lua
+++ testprg/testsca65/neogeo1.lua
@@ -2,7 +2,7 @@ function arrange()
 end
 
 function assert() 
-    res = (read_byte(load_address+3) == 42) and ((read_byte(load_address+4) == 43))
+    res = (read_byte(load_address+3) == 42) and ((read_byte(load_address+4) == 43)) and (read_byte(3*4096) == 0)
     error_msg = ""
 
     if not res then
