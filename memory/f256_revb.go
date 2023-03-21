637ab4769b3786168156562e7ab84f5932b49514
Martin <rmsk2@gmx.de>
2023-03-21 21:49:09 +0100

    Begin to implement Foenix F256 Jr. memory model

--- memory/f256_revb.go
+++ memory/f256_revb.go
@@ -1,8 +1,14 @@
 package memory
 
 type F256RevBMemory struct {
-	mmuMemCtrl *uint8
-	mmuIoCtrl  *uint8
+	mmuMemCtrl uint8
+	mmuIoCtrl  uint8
+
+	mmuMemCtrlSnap uint8
+	mmuIoCtrlSnap  uint8
+
+	accessMmuMemCtrl uint64
+	accessMmuIoCtrl  uint64
 
 	systemMemory []byte
 	ioMemory     []byte
@@ -21,8 +27,11 @@ func NewF56JrMemory() *F256RevBMemory {
 	sysMem := make([]byte, 0x140000)
 
 	res := F256RevBMemory{
-		mmuMemCtrl: &sysMem[0],
-		mmuIoCtrl:  &sysMem[1],
+		mmuMemCtrl: 0,
+		mmuIoCtrl:  0,
+
+		mmuMemCtrlSnap: 0,
+		mmuIoCtrlSnap:  0,
 
 		systemMemory: sysMem,
 		ioMemory:     make([]byte, 32768),
@@ -32,16 +41,29 @@ func NewF56JrMemory() *F256RevBMemory {
 		ioMemorySnap:     make([]byte, 32768),
 		mLutSnap:         make([]byte, 4*8),
 
-		accessSystem: make([]uint64, 0x140000),
-		accessIo:     make([]uint64, 32768),
-		accessMLut:   make([]uint64, 4*8),
+		accessSystem:     make([]uint64, 0x140000),
+		accessIo:         make([]uint64, 32768),
+		accessMLut:       make([]uint64, 4*8),
+		accessMmuMemCtrl: 0,
+		accessMmuIoCtrl:  0,
 	}
 
 	return &res
 }
 
 func (f *F256RevBMemory) calcIndex(addr uint16) (*uint8, *uint64) {
-	return nil, nil
+	switch {
+	case addr == 0:
+		return &f.mmuMemCtrl, &f.accessMmuMemCtrl
+	case addr == 1:
+		return &f.mmuIoCtrl, &f.accessMmuIoCtrl
+	case (addr >= 8) && (addr <= 15):
+		return nil, nil
+	case (addr >= 0xC000) && (addr <= 0xDFFF):
+		return nil, nil
+	default:
+		return nil, nil
+	}
 }
 
 func (f *F256RevBMemory) Load(address uint16) uint8 {
@@ -73,18 +95,25 @@ func (f *F256RevBMemory) ClearStatistics() {
 	for count := 0; count < len(f.accessMLut); count++ {
 		f.accessMLut[count] = 0
 	}
+
+	f.accessMmuMemCtrl = 0
+	f.accessMmuIoCtrl = 0
 }
 
 func (f *F256RevBMemory) TakeSnaphot() {
 	copy(f.systemMemorySnap, f.systemMemory)
 	copy(f.ioMemorySnap, f.ioMemory)
 	copy(f.mLutSnap, f.mLut)
+	f.mmuMemCtrlSnap = f.mmuMemCtrl
+	f.mmuIoCtrlSnap = f.mmuIoCtrl
 }
 
 func (f *F256RevBMemory) RestoreSnapshot() {
 	copy(f.systemMemory, f.systemMemorySnap)
 	copy(f.ioMemory, f.ioMemorySnap)
 	copy(f.mLut, f.mLutSnap)
+	f.mmuMemCtrl = f.mmuMemCtrlSnap
+	f.mmuIoCtrl = f.mmuIoCtrlSnap
 }
 
 func (f *F256RevBMemory) Close() {
