560dbc913b396ed47ae80024633156974dad38f2
Martin <rmsk2@gmx.de>
2023-03-22 21:54:25 +0100

    Complete memory address calculation

--- memory/f256_revb.go
+++ memory/f256_revb.go
@@ -23,8 +23,14 @@ type F256RevBMemory struct {
 	accessMLut   []uint64
 }
 
-func NewF56JrMemory() *F256RevBMemory {
-	sysMem := make([]byte, 0x140000)
+func NewF56JrMemory(expandedMem bool) *F256RevBMemory {
+	memSize := 0x100000
+
+	if expandedMem {
+		memSize = 0x140000
+	}
+
+	sysMem := make([]byte, memSize)
 
 	res := F256RevBMemory{
 		mmuMemCtrl:   0,
@@ -35,13 +41,13 @@ func NewF56JrMemory() *F256RevBMemory {
 
 		mmuMemCtrlSnap:   0,
 		mmuIoCtrlSnap:    0,
-		systemMemorySnap: make([]byte, 0x140000),
+		systemMemorySnap: make([]byte, memSize),
 		ioMemorySnap:     make([]byte, 32768),
 		mLutSnap:         make([]byte, 4*8),
 
 		accessMmuMemCtrl: 0,
 		accessMmuIoCtrl:  0,
-		accessSystem:     make([]uint64, 0x140000),
+		accessSystem:     make([]uint64, memSize),
 		accessIo:         make([]uint64, 32768),
 		accessMLut:       make([]uint64, 4*8),
 	}
@@ -49,18 +55,46 @@ func NewF56JrMemory() *F256RevBMemory {
 	return &res
 }
 
+func (f *F256RevBMemory) calcDefault(activeLut []byte, loBits uint16, hiBits uint16) uint {
+	lower := uint(loBits)
+	var upper uint = (uint(activeLut[hiBits]) << 13)
+
+	return upper | lower
+}
+
 func (f *F256RevBMemory) calcIndex(addr uint16) (*uint8, *uint64) {
+	// lower 13 bits
+	loBits := addr & 0x1FFF
+	hiBits := (addr & 0xE000) >> 13
+	activeLutNum := f.mmuMemCtrl & 0x03
+
+	activeLut := f.mLut[activeLutNum*8 : (activeLutNum+1)*8]
+
 	switch {
 	case addr == 0:
 		return &f.mmuMemCtrl, &f.accessMmuMemCtrl
 	case addr == 1:
 		return &f.mmuIoCtrl, &f.accessMmuIoCtrl
 	case (addr >= 8) && (addr <= 15):
-		return nil, nil
+		if (f.mmuMemCtrl & 0x80) != 0 {
+			editLut := uint16((f.mmuMemCtrl & 0x30) >> 4)
+			return &f.mLut[editLut*8+(addr-8)], &f.accessMLut[editLut*8+(addr-8)]
+		} else {
+			idx := f.calcDefault(activeLut, loBits, hiBits)
+			return &f.systemMemory[idx], &f.accessSystem[idx]
+		}
 	case (addr >= 0xC000) && (addr <= 0xDFFF):
-		return nil, nil
+		if f.mmuIoCtrl&0x04 == 0 {
+			ioBank := uint16(f.mmuIoCtrl & 0x03)
+			idx := ioBank*8192 + addr - 0xC000
+			return &f.ioMemory[idx], &f.accessIo[idx]
+		} else {
+			idx := f.calcDefault(activeLut, loBits, hiBits)
+			return &f.systemMemory[idx], &f.accessSystem[idx]
+		}
 	default:
-		return nil, nil
+		idx := f.calcDefault(activeLut, loBits, hiBits)
+		return &f.systemMemory[idx], &f.accessSystem[idx]
 	}
 }
 
