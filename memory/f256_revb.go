d02eddb9fe491b021a82249a2527f44eb99b43a0
Martin Grap <rmsk2@gmx.de>
2023-03-23 16:39:45 +0100

    Add unit tests for Foenix 256 Jr. memory model

--- memory/f256_revb.go
+++ memory/f256_revb.go
@@ -23,6 +23,11 @@ type F256RevBMemory struct {
 	accessMLut   []uint64
 }
 
+const bankSize uint16 = 8192
+const lutSize = 8
+const numLuts = 4
+const numIoBanks = 4
+
 func NewF56JrMemory(expandedMem bool) *F256RevBMemory {
 	memSize := 0x100000
 
@@ -30,63 +35,77 @@ func NewF56JrMemory(expandedMem bool) *F256RevBMemory {
 		memSize = 0x140000
 	}
 
-	sysMem := make([]byte, memSize)
-
 	res := F256RevBMemory{
 		mmuMemCtrl:   0,
 		mmuIoCtrl:    0,
-		systemMemory: sysMem,
-		ioMemory:     make([]byte, 32768),
-		mLut:         make([]byte, 4*8),
+		systemMemory: make([]byte, memSize),
+		ioMemory:     make([]byte, numIoBanks*bankSize),
+		mLut:         make([]byte, numLuts*lutSize),
 
 		mmuMemCtrlSnap:   0,
 		mmuIoCtrlSnap:    0,
 		systemMemorySnap: make([]byte, memSize),
-		ioMemorySnap:     make([]byte, 32768),
-		mLutSnap:         make([]byte, 4*8),
+		ioMemorySnap:     make([]byte, numIoBanks*bankSize),
+		mLutSnap:         make([]byte, numLuts*lutSize),
 
 		accessMmuMemCtrl: 0,
 		accessMmuIoCtrl:  0,
 		accessSystem:     make([]uint64, memSize),
-		accessIo:         make([]uint64, 32768),
-		accessMLut:       make([]uint64, 4*8),
+		accessIo:         make([]uint64, numIoBanks*bankSize),
+		accessMLut:       make([]uint64, numLuts*lutSize),
 	}
+
+	res.SetMlut(0, []byte{0, 1, 2, 3, 4, 5, 6, 7})
+	res.SetMlut(1, []byte{0, 1, 2, 3, 4, 5, 6, 7})
+	res.SetMlut(2, []byte{0, 1, 2, 3, 4, 5, 6, 7})
+	res.SetMlut(3, []byte{0, 1, 2, 3, 4, 5, 6, 7})
+
+	// MLUT 0 is active
+	// IO bank 0 is active
 
 	return &res
 }
 
 func (f *F256RevBMemory) calcDefault(activeLut []byte, loBits uint16, hiBits uint16) uint {
 	lower := uint(loBits)
-	var upper uint = (uint(activeLut[hiBits]) << 13)
+	var upper uint = (uint(activeLut[hiBits]) << numLoBits)
 
 	return upper | lower
 }
 
+const loBitMask uint16 = 0b0001111111111111
+const numLoBits uint16 = 13
+const lutEditFlagMask uint8 = 0b10000000
+const editLutMask uint8 = 0b00110000
+const activeLutMask uint8 = 0b00000011
+const ioDisableMask uint8 = 0b00000100
+const activeIoBankMask uint8 = 0b00000011
+
 func (f *F256RevBMemory) calcIndex(addr uint16) (*uint8, *uint64) {
 	// lower 13 bits
-	loBits := addr & 0x1FFF
-	hiBits := (addr & 0xE000) >> 13
-	activeLutNum := f.mmuMemCtrl & 0x03
+	loBits := addr & loBitMask
+	hiBits := addr >> numLoBits
+	activeLutNum := f.mmuMemCtrl & activeLutMask
 
-	activeLut := f.mLut[activeLutNum*8 : (activeLutNum+1)*8]
+	activeLut := f.mLut[activeLutNum*lutSize : (activeLutNum+1)*lutSize]
 
 	switch {
 	case addr == 0:
 		return &f.mmuMemCtrl, &f.accessMmuMemCtrl
 	case addr == 1:
 		return &f.mmuIoCtrl, &f.accessMmuIoCtrl
-	case (addr >= 8) && (addr <= 15):
-		if (f.mmuMemCtrl & 0x80) != 0 {
-			editLut := uint16((f.mmuMemCtrl & 0x30) >> 4)
-			return &f.mLut[editLut*8+(addr-8)], &f.accessMLut[editLut*8+(addr-8)]
+	case (addr >= 8) && (addr <= (8 + lutSize - 1)):
+		if (f.mmuMemCtrl & lutEditFlagMask) != 0 {
+			editLut := uint16((f.mmuMemCtrl & editLutMask) >> 4)
+			return &f.mLut[editLut*lutSize+(addr-8)], &f.accessMLut[editLut*lutSize+(addr-8)]
 		} else {
 			idx := f.calcDefault(activeLut, loBits, hiBits)
 			return &f.systemMemory[idx], &f.accessSystem[idx]
 		}
 	case (addr >= 0xC000) && (addr <= 0xDFFF):
-		if f.mmuIoCtrl&0x04 == 0 {
-			ioBank := uint16(f.mmuIoCtrl & 0x03)
-			idx := ioBank*8192 + addr - 0xC000
+		if (f.mmuIoCtrl & ioDisableMask) == 0 {
+			ioBank := uint16(f.mmuIoCtrl & activeIoBankMask)
+			idx := ioBank*bankSize + addr - 0xC000
 			return &f.ioMemory[idx], &f.accessIo[idx]
 		} else {
 			idx := f.calcDefault(activeLut, loBits, hiBits)
@@ -150,4 +169,9 @@ func (f *F256RevBMemory) RestoreSnapshot() {
 
 func (f *F256RevBMemory) Close() {
 
+}
+
+func (f *F256RevBMemory) SetMlut(lutNum uint8, mlutData []byte) {
+	lutNum = lutNum & 0b00000011
+	copy(f.mLut[lutNum*lutSize:(lutNum+1)*lutSize], mlutData)
 }
