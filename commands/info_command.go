18d2821317f60f64b796377b0e774e8b3713fbcb
Martin Grap <rmsk2@gmx.de>
2023-03-24 19:29:05 +0100

    Bump version number

--- commands/info_command.go
+++ commands/info_command.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func InfoCommand(arguments []string) error {
 	fmt.Println("6502 Profiler")
-	fmt.Println("Version 1.0.1")
+	fmt.Println("Version 1.0.2")
 	fmt.Println("Written by Martin Grap (rmsk2@gmx.de) in 2023")
 
 	return nil
